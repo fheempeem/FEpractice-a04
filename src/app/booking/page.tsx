7df331554afb39bcc7ff969d98b715d004d4ddc4
fheempeem <peemplayz@gmail.com>
2026-03-12 16:42:25 +0700

    finish?

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,7 @@
+export default function Booking() {
+    return (
+        <main>
+             <h1>Venue Booking</h1>
+        </main>
+    );
+}
---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import styles from './card.module.css'
+import Image from 'next/image'
+
+export default function Card( {venueName, imgSrc} : {venueName : string, imgSrc : string}) {
+    return (
+        <div className = 'w-1/5 h-[300px] rounded-lg shadow-lg'>
+            <div className = 'w-full h-[70%] relative rounded-t-lg'>
+                <Image src = {imgSrc}
+                    alt = 'Place Picture'
+                    fill = {true}
+                    className='object-cover rounded-t-lg'/>
+            </div>
+            <div className = 'w-full h-[30%] p-[10px]'>
+                {venueName}
+            </div>
+            
+        </div>
+    );
+}
